c6fc52d87c73df84e905839fc875c57b6f14814b
Ashish Shaik <mailashish411@gmail.com>
2026-01-12 00:36:06 -0500

    Remove Secrets.plist from repo

---
+++ PracticeApps/Apps/Pokedex/View/PokemonAppView.swift
@@ -0,0 +1,7 @@
+//
+//  PokedexAppView.swift
+//  PracticeApps
+//
+//  Created by Ashish Shaik on 1/12/26.
+//
+
---
+++ PracticeApps/Apps/Pokedex/View/PokemonDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetailView.swift
+//  PracticeApps
+//
+//  Created by Ashish Shaik on 1/12/26.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonDetailView()
+}
